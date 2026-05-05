b7b4ee5c08d71aae94b84aa71bdaff4a28a24d0d
Jhones Michael <82251374+jhoneshark@users.noreply.github.com>
2026-05-05 00:41:07 -0300

    suport swagger two versions

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using Application;
+using Microsoft.OpenApi.Models;
+
 DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Define o documento para a v1
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minha API", Version = "v1" });
+    
+    // Define o documento para a v2 (Onde está o seu Strategy)
+    c.SwaggerDoc("v2", new OpenApiInfo { Title = "Minha API", Version = "v2" });
+});
 builder.Services.AddCleanArchServices(builder.Configuration);
 
 var app = builder.Build();
@@ -17,7 +26,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1.0");
+        options.SwaggerEndpoint("/swagger/v2/swagger.json", "API v2.0");
+    });
 }
 
 app.UseHttpsRedirection();
